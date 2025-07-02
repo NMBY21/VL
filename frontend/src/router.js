46a633589a725bfc3ab8ee3e4700a251921d849d
NMBY21 <betelihemmengist21@gmail.com>
2025-07-02 10:55:46 +0300

    vl

---
+++ frontend/src/router.js
@@ -0,0 +1,37 @@
+import {createRouter, createWebHistory} from "vue-router";
+import DefaultLayout from "./components/DefaultLayout.vue";
+
+
+const routes = [
+  {
+    path: "/",
+    component: DefaultLayout,
+    children: [
+      {path: '/', name: 'Home', component: Home},
+      {path: '/images', name: 'MyImages', component: MyImages},
+    ],
+  }
+
+   {
+    path: '/login',
+    name: 'Login',
+    component: Login,
+  },
+  {
+    path: '/signup',
+    name: 'Signup',
+    component: Signup,
+  },
+  {
+    path: '/:pathMatch(.*)*',
+    name: 'NotFound',
+    component: NotFound
+  },
+];
+
+const router = createRouter({
+  history: createWebHistory(),
+  routes
+})
+
+export default router
