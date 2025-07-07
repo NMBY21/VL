2705e33f65c3d03800a7b39ebbae7a6eaf10ba0e
NMBY21 <betelihemmengist21@gmail.com>
2025-07-07 18:54:16 +0300

    vl

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -6,4 +6,7 @@ import { createPinia } from "pinia";
 
 const pinia = createPinia();
 
-createApp(App).use(router).use(pinia).mount("#app");
+createApp(App)
+    .use(router)
+    .use(pinia)
+    .mount("#app");
